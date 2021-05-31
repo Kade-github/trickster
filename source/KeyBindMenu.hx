67a78ae097cbac5db52263737fd73e8ae1495235
Kade M <KadePcGames@gmail.com>
2021-05-30 18:23:28 -0700

    pog

---
+++ source/KeyBindMenu.hx
@@ -0,0 +1,268 @@
+package;
+
+/// Code created by Rozebud for FPS Plus (thanks rozebud)
+// modified by KadeDev for use in Kade Engine/Tricky
+
+import Options.Option;
+import flixel.input.FlxInput;
+import flixel.input.keyboard.FlxKey;
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.effects.FlxFlicker;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import io.newgrounds.NG;
+import lime.app.Application;
+import lime.utils.Assets;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.input.FlxKeyManager;
+
+
+using StringTools;
+
+class KeyBindMenu extends MusicBeatState
+{
+
+    var keyTextDisplay:FlxText;
+    var keyWarning:FlxText;
+    var warningTween:FlxTween;
+    var keyText:Array<String> = ["LEFT", "DOWN", "UP", "RIGHT"];
+    var defaultKeys:Array<String> = ["A", "S", "W", "D", "R"];
+    var curSelected:Int = 0;
+
+    var keys:Array<String> = [FlxG.save.data.leftBind,
+                              FlxG.save.data.downBind,
+                              FlxG.save.data.upBind,
+                              FlxG.save.data.rightBind];
+
+    var tempKey:String = "";
+    var blacklist:Array<String> = ["ESCAPE", "ENTER", "BACKSPACE", "SPACE"];
+
+    var state:String = "select";
+
+	override function create()
+	{	
+
+        for (i in 0...keys.length)
+        {
+            var k = keys[i];
+            if (k == null)
+                keys[i] = defaultKeys[i];
+        }
+	
+		//FlxG.sound.playMusic('assets/music/configurator' + TitleState.soundExt);
+
+		persistentUpdate = persistentDraw = true;
+
+		var bg:FlxSprite = new FlxSprite(-10,-10).loadGraphic(Paths.image('menu/freeplay/RedBG','clown'));
+		add(bg);
+		var hedge:FlxSprite = new FlxSprite(-810,-335).loadGraphic(Paths.image('menu/freeplay/hedge','clown'));
+		hedge.setGraphicSize(Std.int(hedge.width * 0.65));
+		add(hedge);
+		var shade:FlxSprite = new FlxSprite(-205,-100).loadGraphic(Paths.image('menu/freeplay/Shadescreen','clown'));
+		shade.setGraphicSize(Std.int(shade.width * 0.65));
+		add(shade);
+		var bars:FlxSprite = new FlxSprite(-225,-395).loadGraphic(Paths.image('menu/freeplay/theBox','clown'));
+		bars.setGraphicSize(Std.int(bars.width * 0.65));
+		add(bars);
+
+        keyTextDisplay = new FlxText(-10, 0, 1280, "", 72);
+		keyTextDisplay.scrollFactor.set(0, 0);
+		keyTextDisplay.setFormat("tahoma-bold.ttf", 54, FlxColor.RED, FlxTextAlign.CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		keyTextDisplay.borderSize = 3;
+		keyTextDisplay.borderQuality = 1;
+        add(keyTextDisplay);
+
+        keyWarning = new FlxText(0, 580, 1280, "WARNING: BIND NOT SET, TRY ANOTHER KEY", 42);
+		keyWarning.scrollFactor.set(0, 0);
+		keyWarning.setFormat("vcr.ttf", 42, FlxColor.WHITE, FlxTextAlign.CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+        keyWarning.borderSize = 3;
+		keyWarning.borderQuality = 1;
+        keyWarning.screenCenter(X);
+        keyWarning.alpha = 0;
+        add(keyWarning);
+
+        warningTween = FlxTween.tween(keyWarning, {alpha: 0}, 0);
+
+        textUpdate();
+
+		super.create();
+	}
+
+	override function update(elapsed:Float)
+	{
+
+        switch(state){
+
+            case "select":
+                if (FlxG.keys.justPressed.UP)
+				{
+					FlxG.sound.play(Paths.sound('Hover','clown'));
+					changeItem(-1);
+				}
+
+				if (FlxG.keys.justPressed.DOWN)
+				{
+					FlxG.sound.play(Paths.sound('Hover','clown'));
+					changeItem(1);
+				}
+
+                if (FlxG.keys.justPressed.ENTER){
+                    FlxG.sound.play(Paths.sound('Hover','clown'));
+                    state = "input";
+                }
+                else if(FlxG.keys.justPressed.ESCAPE){
+                    FlxG.sound.play(Paths.sound('confirm','clown'));
+                    quit();
+                }
+				else if (FlxG.keys.justPressed.BACKSPACE){
+                    FlxG.sound.play(Paths.sound('confirm','clown'));
+                    reset();
+                }
+
+            case "input":
+                tempKey = keys[curSelected];
+                keys[curSelected] = "?";
+                textUpdate();
+                state = "waiting";
+
+            case "waiting":
+                if(FlxG.keys.justPressed.ESCAPE){
+                    keys[curSelected] = tempKey;
+                    state = "select";
+                    FlxG.sound.play(Paths.sound('confirm','clown'));
+                }
+                else if(FlxG.keys.justPressed.ENTER){
+                    addKey(defaultKeys[curSelected]);
+                    save();
+                    state = "select";
+                }
+                else if(FlxG.keys.justPressed.ANY){
+                    addKey(FlxG.keys.getIsDown()[0].ID.toString());
+                    save();
+                    state = "select";
+                }
+
+
+            case "exiting":
+
+
+            default:
+                state = "select";
+
+        }
+
+        if(FlxG.keys.justPressed.ANY)
+			textUpdate();
+
+		super.update(elapsed);
+		
+	}
+
+    function textUpdate(){
+
+        keyTextDisplay.text = "\n\n";
+
+        for(i in 0...4){
+
+            var textStart = (i == curSelected) ? "> " : "  ";
+            keyTextDisplay.text += textStart + keyText[i] + ": " + ((keys[i] != keyText[i]) ? (keys[i] + " / ") : "" ) + keyText[i] + " ARROW\n";
+
+        }
+
+        keyTextDisplay.screenCenter();
+
+    }
+
+    function save(){
+
+        FlxG.save.data.upBind = keys[2];
+        FlxG.save.data.downBind = keys[1];
+        FlxG.save.data.leftBind = keys[0];
+        FlxG.save.data.rightBind = keys[3];
+
+        FlxG.save.flush();
+
+        PlayerSettings.player1.controls.loadKeyBinds();
+
+    }
+
+    function reset(){
+
+        for(i in 0...5){
+            keys[i] = defaultKeys[i];
+        }
+        quit();
+
+    }
+
+    function quit(){
+
+        state = "exiting";
+
+        save();
+
+        FlxG.switchState(new OptionsMenu());
+
+    }
+
+	function addKey(r:String){
+
+        var shouldReturn:Bool = true;
+
+        var notAllowed:Array<String> = [];
+
+        for(x in keys){
+            if(x != tempKey){notAllowed.push(x);}
+        }
+
+        for(x in blacklist){notAllowed.push(x);}
+
+        if(curSelected != 4){
+
+            for(x in keyText){
+                if(x != keyText[curSelected]){notAllowed.push(x);}
+            }
+            
+        }
+        else {for(x in keyText){notAllowed.push(x);}}
+
+        trace(notAllowed);
+
+        for(x in 0...keys.length)
+            {
+                var oK = keys[x];
+                if(oK == r)
+                    keys[x] = null;
+            }
+
+        if(shouldReturn){
+            keys[curSelected] = r;
+            FlxG.sound.play(Paths.sound('Hover','clown'));
+        }
+        else{
+            keys[curSelected] = tempKey;
+            FlxG.sound.play(Paths.sound('confirm','clown'));
+            keyWarning.alpha = 1;
+            warningTween.cancel();
+            warningTween = FlxTween.tween(keyWarning, {alpha: 0}, 0.5, {ease: FlxEase.circOut, startDelay: 2});
+        }
+
+	}
+
+    function changeItem(_amount:Int = 0)
+    {
+        curSelected += _amount;
+                
+        if (curSelected > 3)
+            curSelected = 0;
+        if (curSelected < 0)
+            curSelected = 3;
+    }
+}
